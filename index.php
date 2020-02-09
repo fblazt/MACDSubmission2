df4e359d1da7345f72af14dd2f60853286c2a952
fblazt <48191467+fblazt@users.noreply.github.com>
2020-02-09 21:54:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
     use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 
-    $connectionString = "AccountName=macdstorage;AccountKey=u6bmfai3WQKPRxDfHR4iL0c1g8CZlNDywVahvdR5tqIBFHhCizCgwSviVXIj+mabLkPL/WndWLA7jvlwRWEMXQ==";
+    $connectionString = "DefaultEndpointsProtocol=https;AccountName=macdstorage;AccountKey=u6bmfai3WQKPRxDfHR4iL0c1g8CZlNDywVahvdR5tqIBFHhCizCgwSviVXIj+mabLkPL/WndWLA7jvlwRWEMXQ==;EndpointSuffix=core.windows.net";
     
     $blobClient = BlobRestProxy::createBlobService($connectionString);
 
